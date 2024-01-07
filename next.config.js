a203e09be20a534078beb49602293595a9af688e
Rizal Priyono <rizal10priyono11@gmail.com>
2024-01-07 17:32:59 +0700

    implement pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// Configuration options for Next.js
+const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+})
+
+// Export the combined configuration for Next.js with PWA support
+module.exports = withPWA(nextConfig)
