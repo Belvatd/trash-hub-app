10742f1768b52b338f8cb808c9917b8413fb9643
Rizal Priyono <rizal10priyono11@gmail.com>
2024-01-09 18:32:55 +0700

    adjust dvh

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react"
 const Layout = (props: PropsWithChildren) => {
   return (
     <div
-      className="flex h-screen w-full bg-slate-200 bg-cover p-2 pb-4"
+      className="flex h-full w-full bg-cover p-2 pb-4"
       style={{ backgroundImage: "url(https://i.ibb.co/3c0P6T7/Bg.png)" }}
     >
       {props.children}
